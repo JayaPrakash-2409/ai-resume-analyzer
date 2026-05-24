259c608d947e955cb33b19215adc0ce75b21dbd8
JayaPrakash-2409 <jayaprakashnarayana94@gmail.com>
2026-05-24 10:50:43 +0530

    Added resume upload analyzer

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-from flask import Flask
+from flask import Flask, request
+from pdfminer.high_level import extract_text
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        file = request.files["resume"]
+
+        filepath = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(filepath)
+
+        text = extract_text(filepath)
+
+        return f"""
+        <h1>Resume Uploaded Successfully 🚀</h1>
+        <h3>Extracted Resume Text:</h3>
+        <pre>{text[:3000]}</pre>
+        """
+
     return """
     <h1>AI Resume Analyzer 🚀</h1>
-    <p>Successfully deployed on Vercel</p>
+
+    <form method="POST" enctype="multipart/form-data">
+        <input type="file" name="resume" required>
+        <button type="submit">Analyze Resume</button>
+    </form>
     """
 
 handler = app
